5b31002c8c02e207e1597b3d4d9a1936576bfe37
VladoozES <V-moravec@mail.ru>
2021-04-15 16:11:22 +0500

    Lara face is successed

--- Lara/Lara/Form1.cs
+++ Lara/Lara/Form1.cs
@@ -28,40 +28,117 @@ namespace Lara
             gL.Color(1f, 1f, 1f);
             gL.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
 
-            ///Общие точки///
-            float[] rightCheeckFarPoint = new float[] { -1.0f, 0.7f, 0.0f };
-            float[] rightCheeckNearPoint = new float[] { -0.8f, 0.6f, 0.5f };
-            float[] rightTempleAndForehead = new float[] { -0.8f, 1f, 0.4f };
-            float[] lowerCenterOfForehead = new float[] { -0.4f, 1f, 0.6f};
-            float[] faceCenter = new float[] { -0.4f, 0.6f, 0.6f};
+            
 
-            //Лицо Лары
+            //Лицо Лары по XY
             gL.LoadIdentity();
-            gL.Translate(0.0f, 0.0f, -6.0f);
+            gL.Translate(-1.5f, 1.0f, -6.0f);
+            gL.Rotate(0.0, 0.0f, 1.0f, 0.0f);
+            DrawLaraFace(gL);//*/
+
+            //Лицо Лары по YZ
+            gL.LoadIdentity();
+            gL.Translate(-1.5f, -1.0f, -6.0f);
+            gL.Rotate(90, 0.0f, 1.0f, 0.0f);
+            DrawLaraFace(gL);//*/
+
+            //Лицо Лары вращается
+            gL.LoadIdentity();
+            gL.Translate(1.0f, 0.0f, -6.0f);
             gL.Rotate(rotateAngle, 0.0f, 1.0f, 0.0f);
+            DrawLaraFace(gL);
+
+
+
+
+
+
+            rotateAngle += 3f;
+        }
+
+        private void DrawLaraFace(OpenGL gL)
+        {
+            ///Общие точки///
+            float[] rightCheeckFarPoint = new float[] { -0.6f, 0.7f, 0.0f };    //Дальняя верхняя точка щеки
+            float[] leftCheeckFarPoint = new float[] { 0.6f, 0.7f, 0.0f };
+
+            float[] rightCheeckNearPoint = new float[] { -0.4f, 0.4f, 0.4f };   //Ближняя верхняя точка щеки
+            float[] leftCheeckNearPoint = new float[] { 0.4f, 0.4f, 0.4f };
+
+            float[] rightFarLowerCheek = new float[] { -0.4f, -0.05f, 0.0f }; //Дальняя нижняя точка щеки
+            float[] leftFarLowerCheek = new float[] { 0.4f, -0.05f, 0.0f };
+
+            float[] rightTempleAndForehead = new float[] { -0.4f, 1f, 0.4f };   //Висок примерно
+            float[] leftTempleAndForehead = new float[] { 0.4f, 1f, 0.4f };
+
+            float[] rightForeheadRightHightPoint = new float[] { -0.35f, 1.2f, 0.35f }; //Дальняя верхняя точка лба
+            float[] leftForeheadRightHightPoint = new float[] { 0.35f, 1.2f, 0.35f };
+
+            float[] rightChin = new float[] { -0.2f, -0.4f, 0.55f }; //Подбородок
+            float[] leftChin = new float[] { 0.2f, -0.4f, 0.55f };
+
+            float[] lowerCenterOfForehead = new float[] { 0.0f, 1f, 0.8f }; //Нижний центр лба
+            float[] highCenterOfForehead = new float[] { 0.0f, 1.2f, 0.6f };    //Верхний центр лба
+            float[] faceCenter = new float[] { 0.0f, 0.4f, 0.85f }; //Самый центр лица
+            float[] faceLowerPoint = new float[] { 0.0f, -0.4f, 0.8f }; //Низ центра лица
+
+
+
+            ///Точки для волос///
+            //float[] 
+
+
+
+
             gL.Begin(OpenGL.GL_QUADS);  // Деталь 1
                     gL.Vertex(rightCheeckFarPoint[0], rightCheeckFarPoint[1], rightCheeckFarPoint[2]);
-                    gL.Vertex(-1.0f, 0.0f, 0.0f);
-                    gL.Vertex(-0.8f, -0.4f, 0.5f);
+                    gL.Vertex(rightFarLowerCheek[0], rightFarLowerCheek[1], rightFarLowerCheek[2]);
+                    gL.Vertex(rightChin[0], rightChin[1], rightChin[2]);
                     gL.Vertex(rightCheeckNearPoint[0], rightCheeckNearPoint[1], rightCheeckNearPoint[2]);
+            gL.End();
+            gL.Begin(OpenGL.GL_QUADS);  // Зеркало деталь 1
+                    gL.Vertex(leftCheeckFarPoint[0], leftCheeckFarPoint[1], leftCheeckFarPoint[2]);
+                    gL.Vertex(leftFarLowerCheek[0], leftFarLowerCheek[1], leftFarLowerCheek[2]);
+                    gL.Vertex(leftChin[0], leftChin[1], leftChin[2]);
+                    gL.Vertex(leftCheeckNearPoint[0], leftCheeckNearPoint[1], leftCheeckNearPoint[2]);
             gL.End();
             gL.Begin(OpenGL.GL_TRIANGLES); // Деталь 2
                     gL.Vertex(rightCheeckFarPoint[0], rightCheeckFarPoint[1], rightCheeckFarPoint[2]);
                     gL.Vertex(rightCheeckNearPoint[0], rightCheeckNearPoint[1], rightCheeckNearPoint[2]);
                     gL.Vertex(rightTempleAndForehead[0], rightTempleAndForehead[1], rightTempleAndForehead[2]);
             gL.End();
+            gL.Begin(OpenGL.GL_TRIANGLES); // Зеркало деталь 2
+                    gL.Vertex(leftCheeckFarPoint[0], leftCheeckFarPoint[1], leftCheeckFarPoint[2]);
+                    gL.Vertex(leftCheeckNearPoint[0], leftCheeckNearPoint[1], leftCheeckNearPoint[2]);
+                    gL.Vertex(leftTempleAndForehead[0], leftTempleAndForehead[1], leftTempleAndForehead[2]);
+            gL.End();
             gL.Begin(OpenGL.GL_QUAD_STRIP); // Деталь 3
                     gL.Vertex(rightTempleAndForehead[0], rightTempleAndForehead[1], rightTempleAndForehead[2]);
                     gL.Vertex(lowerCenterOfForehead[0], lowerCenterOfForehead[1], lowerCenterOfForehead[2]);
                     gL.Vertex(rightCheeckNearPoint[0], rightCheeckNearPoint[1], rightCheeckNearPoint[2]);
                     gL.Vertex(faceCenter[0], faceCenter[1], faceCenter[2]);
-                    gL.Vertex(-0.8f, -0.4f, 0.5f);
-                    gL.Vertex(-0.4f, -0.4f, 0.6f);
+                    gL.Vertex(rightChin[0], rightChin[1], rightChin[2]);
+                    gL.Vertex(faceLowerPoint[0], faceLowerPoint[1], faceLowerPoint[2]);
             gL.End();
-
-
-
-            rotateAngle += 3f;
+            gL.Begin(OpenGL.GL_QUAD_STRIP); // Зеркало деталь 3
+                    gL.Vertex(leftTempleAndForehead[0], leftTempleAndForehead[1], leftTempleAndForehead[2]);
+                    gL.Vertex(lowerCenterOfForehead[0], lowerCenterOfForehead[1], lowerCenterOfForehead[2]);
+                    gL.Vertex(leftCheeckNearPoint[0], leftCheeckNearPoint[1], leftCheeckNearPoint[2]);
+                    gL.Vertex(faceCenter[0], faceCenter[1], faceCenter[2]);
+                    gL.Vertex(leftChin[0], leftChin[1], leftChin[2]);
+                    gL.Vertex(faceLowerPoint[0], faceLowerPoint[1], faceLowerPoint[2]);
+            gL.End();
+            gL.Begin(OpenGL.GL_QUADS); // Деталь 4
+                    gL.Vertex(rightTempleAndForehead[0], rightTempleAndForehead[1], rightTempleAndForehead[2]);
+                    gL.Vertex(lowerCenterOfForehead[0], lowerCenterOfForehead[1], lowerCenterOfForehead[2]);
+                    gL.Vertex(highCenterOfForehead[0], highCenterOfForehead[1], highCenterOfForehead[2]);
+                    gL.Vertex(rightForeheadRightHightPoint[0], rightForeheadRightHightPoint[1], rightForeheadRightHightPoint[2]);
+            gL.End(); gL.Begin(OpenGL.GL_QUADS); // Зеркало деталь 4
+                    gL.Vertex(leftTempleAndForehead[0], leftTempleAndForehead[1], leftTempleAndForehead[2]);
+                    gL.Vertex(lowerCenterOfForehead[0], lowerCenterOfForehead[1], lowerCenterOfForehead[2]);
+                    gL.Vertex(highCenterOfForehead[0], highCenterOfForehead[1], highCenterOfForehead[2]);
+                    gL.Vertex(leftForeheadRightHightPoint[0], leftForeheadRightHightPoint[1], leftForeheadRightHightPoint[2]);
+            gL.End();
         }
     }
 }
